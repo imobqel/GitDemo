285ee596093d08ce1719eca1fadf4df890ed29e6
imoqbel <imran.moqbel@alsahabglobal.com>
2020-04-18 16:20:51 +0300

    commit by GitDemo2

--- src/Test2.java
+++ src/Test2.java
@@ -1,10 +1,12 @@
 
 public class Test2 {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Switched to develop branch");
-		
+
+		System.out.println("a new test by GitDemo2");
+
 	}
 
 }
